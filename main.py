51a99258481e78e18cf38a1e78ba43e703811203
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-12-11 14:13:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 '''
-Crie um programa que leia a quantidade de combustível restante de um veículo em L, o consumo médio de combustível do veículo e a distância dos cinco postos de gasolina mais próximos em Km. Considere que a quantidade de combustível restante do veículo é de 2 L, cada litro de combustível permite que o veículo rode 8 Km e que as distâncias até os cinco postos de gasolina mais próximas são 2, 15, 22 e 10.2.
+Crie um programa que leia a quantidade de combustível restante de um veículo em litros, o consumo médio de combustível do veículo em quilometros por litro e a distância dos cinco postos de gasolina mais próximos em quilometros. Considere as distâncias até os cinco postos de gasolina mais próximas são 2, 15, 22 e 10.2.
 '''
 
-c = float(input('Digite a quantidade de combustível do veículo: '))
+c = float(input('Digite a quantidade de combustível atual do veículo: '))
+km = float(input('Digite quantos quilometros o veículo é capaz de rodar a cada litro de gasolina: '))
+
 d = [2, 15, 22, 10.2]
+
 n = -1
 
 d.sort(reverse=True)
 
 for i in range(0, len(d)):
-    g = d[i] / 8
+    g = d[i] / km
 
     if g <= c and d[i] > n:
         n = d[i]
