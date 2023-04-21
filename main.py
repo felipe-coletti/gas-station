f6a6086ab96e01246098fa70e588b3cfcc939098
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-21 20:32:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 Crie um programa que leia a quantidade de combustível restante de um veículo em litros, o consumo médio de combustível do veículo em quilometros por litro e mostre o posto de gasolina mais distante dentro do limite de combustível do veículo. Considere que a distância dos quatro postos de gasolina mais próximos em quilometros é de 2, 15, 22 e 10,2.
 '''
 
-availableFuel = float(input('Digite a quantidade de combustível do veículo em litros: '))
+fuel = float(input('Digite a quantidade de combustível do veículo em litros: '))
 averageConsumption = float(input('Digite o consumo médio de combustível do veículo em quilômetros por litro: '))
 
-availableDistances = [2, 15, 22, 10.2]
+distances = [2, 15, 22, 10.2]
 
-availableDistances.sort()
+distances.sort()
 
-for distance in availableDistances:
-    requiredFuel = distance / averageConsumption
+distanceLimit = fuel * averageConsumption
 
-    if requiredFuel <= availableFuel:
+longestPossibleDistance = -1
+
+for distance in distances:
+    if distance <= distanceLimit:
         longestPossibleDistance = distance
 
 if longestPossibleDistance >= 0:
